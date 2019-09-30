a65d6ce42c3c438c233391bd50bd0cefa88c8e4d
Tajbir Sandhu <sandhu_tajbir@outlook.com>
2019-09-30 01:19:25 -0700

    Add project files.

---
+++ GymMembers/Model/MemberDB.cs
@@ -0,0 +1,74 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMembers.Model
+{
+    class MemberDB : ObservableObject
+    {
+        /// <summary>
+        /// The list of members to be saved.
+        /// </summary>
+        private ObservableCollection<Member> members;
+        /// <summary>
+        /// Where the database is stored.
+        /// </summary>
+        private string filepath = Path.Combine(Path.GetDirectoryName((Path.GetDirectoryName(Directory.GetCurrentDirectory()))), "members.txt");
+
+        /// <summary>
+        /// Creates a new member database.
+        /// </summary>
+        /// <param name="m">The list to saved from or written to.</param>
+        public MemberDB(ObservableCollection<Member> m)
+        {
+            members = m;
+        }
+
+        /// <summary>
+        /// Reads the saved text file database into the program's list of members.
+        /// </summary>
+        /// <returns>The list containing the text file data read in.</returns>
+        public ObservableCollection<Member> GetMemberships()
+        {
+            try
+            {
+                string line;
+                StreamReader input = new StreamReader(new FileStream(filepath, FileMode.OpenOrCreate, FileAccess.Read));
+                while ((line = input.ReadLine()) != null)
+                {
+                    string[] spearator = { " ", ", " };
+                    string[] data = line.Split(spearator, 3, StringSplitOptions.RemoveEmptyEntries);
+                    members.Add(new Member(data[0], data[1], data[2]));
+                }
+                input.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid e-mail address format.");
+            }
+            return members;
+        }
+
+        /// <summary>
+        /// Saves the program's list of members into the text file database.
+        /// </summary>
+        public void SaveMemberships()
+        {
+            StreamWriter output = new StreamWriter(new FileStream(filepath, FileMode.Create, FileAccess.Write));
+            foreach (Member m in members)
+            {
+                output.WriteLine(m);
+            }
+            output.Close();
+        }
+    }
+}
